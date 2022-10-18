91dcd048912c30a31c0f72bb2e52b8d13b647a89
Viktoriia Moskalenko <v.moskalenkoda@gmail.com>
2022-10-18 11:48:58 +0200

    add button[0]

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
+//.getElementByClassName always return an array of elements. 
+//To make it work when we call it, call as array element 
+//button[0].addEventListener('click', add)
+var button = document.getElementsByClassName("btn");
+var input = document.getElementById("address");
+var answ = document.getElementById("answer");
 
-let button = document.getElementsByClassName("btn");
-let input = document.getElementById("address");
-let answ = document.getElementById("answer");
-
-function add() 
+function add(e) 
 {
+  e.preventDefault();
   answ.innerHTML = "Submited";
 };
-button.addEventListener("click", add)
+button[0].addEventListener('click', add);
+
 
 
 var items = document.getElementsByClassName("fade-item");
-		for (let i = 0; i < items.length; ++i) {
-      fadeIn(items[i], i * 1000)
-    }
-    function fadeIn (item, delay) {
-      setTimeout(() => {
-        item.classList.add('fadein')
-      }, delay)
-    }
+for (let i = 0; i < items.length; ++i) {
+  fadeIn(items[i], i * 1000)
+}
+function fadeIn(item, delay) {
+  setTimeout(() => {
+    item.classList.add('fadein')
+  }, delay)
+}
 
-   
+
 
 
